e22eb94d19c80a0986d5537c668dd2b70934d6dd
刘卿 <lqing@jointwisdom.cn>
2018-11-01 14:00:45 +0800

    webpack add async chunks

--- webpack.config.js
+++ webpack.config.js
@@ -112,14 +112,43 @@ module.exports = {
     ]
   },
   optimization: {
+    //SplitChunksPlugin配置，其中缓存组概念目前不是很清楚
     splitChunks: {
+      // 表示显示块的范围，有三个可选值：initial(初始块)、async(按需加载块)、all(全部块)，默认为all;
+      chunks: "async",
+      // 表示在压缩前的最小模块大小，默认为0；
+      minSize: 3000,
+      //表示被引用次数，默认为1
+      minChunks: 1,
+      //最大的按需(异步)加载次数，默认为1；
+      maxAsyncRequests: 3,
+      //最大的初始化加载次数，默认为1；
+      maxInitialRequests: 3,
+      // 拆分出来块的名字(Chunk Names)，默认由块名和hash值自动生成；设置ture则使用默认值
+      name: true,
+      //缓存组，目前在项目中设置cacheGroup可以抽取公共模块，不设置则不会抽取
       cacheGroups: {
+        //缓存组信息，名称可以自己定义
         commons: {
-          chunks: 'initial',
-          minChunks: 2,
-          maxInitialRequests: 5, // The default limit is too small to showcase the effect
-          minSize: 0, // This is example is too small to create commons chunks
-          name: 'common'
+          //拆分出来块的名字,默认是缓存组名称+"~" + [name].js
+          // name: "test",
+          // 同上
+          // chunks: "all",
+          // 同上
+          // minChunks: 3,
+          // 如果cacheGroup中没有设置minSize，则据此判断是否使用上层的minSize，true：则使用0，false：使用上层minSize
+          enforce: true,
+          //test: 缓存组的规则，表示符合条件的的放入当前缓存组，值可以是function、boolean、string、RegExp，默认为空；
+          test: ""
+        },
+        //设置多个缓存规则
+        vendor: {
+          test: /node_modules/,
+          chunks: "all",
+          name: "vendor",
+          //表示缓存的优先级
+          priority: 10,
+          enforce: true
         }
       }
     }
